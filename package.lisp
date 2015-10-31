7b6ee80698faf84f07ad2571ad985c04413e0c71
Zach Beane <xach@xach.com>
2015-10-30 20:38:31 -0400

    First pass at exporting required functionality from the package.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,30 @@
 (defpackage #:funweb
   (:use #:cl)
   (:nicknames #:fw)
+  (:export #:configuredp
+           #:configure
+           #:configure-from-file
+           #:object-not-configured)
+  (:export #:host
+           #:port
+           #:http-method)
+  (:export #:*request*
+           #:url-path
+           #:parameter-value
+           #:header-value
+           #:request-property)
+  (:export #:start-server
+           #:stop-server)
   (:export #:define-app
-           #:define-handler))
+           #:define-handler
+           #:app-data
+           #:fill-template
+           #:template-values
+           #:make-output-file
+           #:path
+           #:file
+           #:app
+           #:no-base-directory
+           #:base-directory
+           #:relative-to))
 
